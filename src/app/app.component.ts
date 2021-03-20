5b64f5c62bb03e664881b3373544db1b29373ca2
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-03-19 23:16:44 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+
+  images = [
+    {
+      title: 'Na praia', 
+      url: 'https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Praia Brava',
+      url: 'https://images.unsplash.com/photo-1611148799269-63df34f63f6c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cHJhaWElMjBicmF2YXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Praia do Sancho',
+      url: 'https://images.unsplash.com/photo-1593193391560-76ce8e2b313c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cHJhaWElMjBkbyUyMHNhbmNob3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Praia do Leão',
+      url: 'https://images.unsplash.com/photo-1553894506-d9ff27d44a90?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8cHJhaWElMjBkbyUyMGxlJUMzJUEzb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+  ];
+
 }
